887244a8322128113b9f67c4ed0524fce4c85b8d
MarechalBanane <faure_g@hotmail.com>
2014-01-25 01:39:50 +0100

    exporter : first version finished
    c'est la fête

--- exporter.js
+++ exporter.js
@@ -1,26 +1,34 @@
 var fs = require('fs');
-var input = fs.readFileSync("test.csv", 'utf8');
-var output = "[\n\t[\"";
-for (var i = 0; i < input.length - 1; ++i) {
-  var currentChar = input.charAt(i);
-  switch (currentChar)
-  {
-	case ';':
-	  output += "\",\"";
-	  break;
-	case '\n':
-	  output += "\"],\n\t[\"";
-	case ' ':
-	case '\t':
-	case '\r':
-	  break;
-	default:
-	  output += currentChar;
-      break;
-  }
-}
-output += "\"]\n]";
+var files = fs.readdirSync(".");
 
-console.log(input);
-console.log(output);
-fs.writeFileSync("test.json", output, 'utf8');
+files.forEach(function(filename) {
+	var array = filename.split(".");
+	if (array.length > 1 && array[1] == "csv") {
+		generateLevelJS(filename);
+	}
+});
+
+function generateLevelJS(filename) {
+	var input = fs.readFileSync(filename, 'utf8');
+	var output = "exports.LVL_" + filename.substring(0, filename.length -4) +  " = [\n\t[\"";
+	for (var i = 0; i < input.length - 1; ++i) {
+		var currentChar = input.charAt(i);
+		switch (currentChar)
+		{
+		case ';':
+			output += "\",\"";
+			break;
+		case '\n':
+			output += "\"],\n\t[\"";
+		case ' ':
+		case '\t':
+		case '\r':
+			break;
+		default:
+			output += currentChar;
+			break;
+		}
+	}
+	output += "\"]\n];";
+	fs.writeFileSync("LVL_" + filename.substring(0, filename.length -4) + ".js", output, 'utf8');
+}
