2908aa4f76612cb0e17d31e49478370aa785bddf
MarechalBanane <faure_g@hotmail.com>
2014-01-25 16:14:16 +0100

    new levels by Keke

--- app.js
+++ app.js
@@ -20,7 +20,15 @@ var ws = require('ws');
 var app = express();
 
 var levels = [];
+levels.push("LVL_1.js");
 levels.push("LVL_2.js");
+levels.push("LVL_3.js");
+levels.push("LVL_4.js");
+levels.push("LVL_5.js");
+levels.push("LVL_6.js");
+levels.push("LVL_7.js");
+levels.push("LVL_8.js");
+levels.push("LVL_9.js");
 
 function genMap() {
   var map = require("./" + levels[Math.floor(Math.random()*levels.length)]).level;
